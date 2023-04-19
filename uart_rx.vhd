3645af443c4d1b545985bc24578b10b240658358
Martan03 <martinslezak03@gmail.com>
2023-04-19 10:55:36 +0200

    Add vhdl files

---
+++ uart_rx.vhd
@@ -0,0 +1,43 @@
+-- uart_rx.vhd: UART controller - receiving (RX) side
+-- Author(s): Name Surname (xlogin00)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+
+
+-- Entity declaration (DO NOT ALTER THIS PART!)
+entity UART_RX is
+    port(
+        CLK      : in std_logic;
+        RST      : in std_logic;
+        DIN      : in std_logic;
+        DOUT     : out std_logic_vector(7 downto 0);
+        DOUT_VLD : out std_logic
+    );
+end entity;
+
+
+
+-- Architecture implementation (INSERT YOUR IMPLEMENTATION HERE)
+architecture behavioral of UART_RX is
+    -- Defining counters
+    signal cnt16 : std_logic_vector(3 downto 0);
+    signal cnt8  : std_logic_vector(2 downto 0);
+begin
+
+    -- Instance of RX FSM
+    fsm: entity work.UART_RX_FSM
+    port map (
+        CLK => CLK,
+        RST => RST,
+        DIN => DIN,
+        READ_DONE => '0',
+        WAIT_DONE => '0'
+    );
+
+    DOUT <= (others => '0');
+    DOUT_VLD <= '0';
+
+end architecture;
---
+++ uart_rx_fsm.vhd
@@ -0,0 +1,78 @@
+-- uart_rx_fsm.vhd: UART controller - finite state machine controlling RX side
+-- Author(s): Martin Slezák (xsleza26)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+
+
+entity UART_RX_FSM is
+    port(
+        CLK : in std_logic;
+        RST : in std_logic;
+        DIN : in std_logic;
+        READ_DONE : in std_logic;
+        WAIT_DONE : in std_logic;
+        RX_WAIT : out std_logic;
+        RX_OFFSET : out std_logic;
+        RX_FIRST : out std_logic;
+        RX_READ : out std_logic;
+        RX_END : out std_logic
+    );
+end entity;
+
+
+
+architecture behavioral of UART_RX_FSM is
+    -- FSM states
+    type state_t is (
+        WAIT_START,
+        WAIT_OFFSET,
+        WAIT_FIRST,
+        WAIT_READ,
+        WAIT_END,
+        DATA_VALID
+    );
+    signal state : state_t := WAIT_START;
+begin
+    process (CLK) begin
+        -- Detect rising edge only
+        if not rising_edge(CLK) then
+            return;
+        end if;
+
+        -- Reset state
+        if RST = '1' then
+            state <= WAIT_START;
+            return;
+        end if;
+
+        -- Moving to next state
+        case state is
+            when WAIT_START =>
+                if DIN = '0' then
+                    state <= WAIT_OFFSET;
+                end if;
+            when WAIT_OFFSET =>
+                if READ_DONE = '1' then
+                    state <= WAIT_FIRST;
+                end if;
+            when WAIT_FIRST =>
+                if WAIT_DONE = '1' then
+                    state <= WAIT_READ;
+                end if;
+            when WAIT_READ =>
+                if (WAIT_DONE = '1') and (WAIT_DONE = '1') then
+                    state <= WAIT_END;
+                end if;
+            when WAIT_END =>
+                if DIN = '1' then
+                    state <= DATA_VALID;
+                end if;
+            when DATA_VALID =>
+                state <= WAIT_START;
+            when others => null;
+        end case;
+    end process;
+end architecture;
